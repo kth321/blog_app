51b197462fc6d5765ba74d3c7547922accc63ae9
kth321 <rnxogud136@naver.com>
2021-06-13 13:25:47 +0000

    modify blog app model =>add author field, published_date field

--- Blogsite/blog/models.py
+++ Blogsite/blog/models.py
@@ -3,5 +3,10 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50, null=True)
     body = models.TextField()
     photo = models.ImageField(null=True, blank=True)
+    published_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.title
