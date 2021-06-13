5309f9e4316fa068c57e829d8419bace9a34e566
kth321 <rnxogud136@naver.com>
2021-06-13 14:40:00 +0000

    change static file url and make background-image for base.html

--- Blogsite/Blogsite/settings.py
+++ Blogsite/Blogsite/settings.py
@@ -122,9 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    ('blog', os.path.join(BASE_DIR, 'static'), ),
+    os.path.join(BASE_DIR, 'static')
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
